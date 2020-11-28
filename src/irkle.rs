89b770d4e98739f3fb2747c0201d6679cbe9809b
Owez <root@ogriffiths.com>
2020-11-28 17:16:19 +0000

    Split into seperate files

---
+++ src/irkle.rs
@@ -0,0 +1,63 @@
+//! Contains [Irkle], see item-level documentation for more information
+
+use crate::Node;
+use std::rc::Weak;
+
+/// The primary tree used inside of irkle, uses an [array-based](https://en.wikipedia.org/wiki/Binary_tree#Arrays)
+/// method of storage
+///
+/// # Tree structure
+///
+/// Given a tree looking similar to the following:
+///
+/// ```none
+/// alpha
+///     bravo
+///         charlie
+///         delta
+///     echo
+///         foxtrot
+///         golf
+/// ```
+///
+/// It would be translated into a list similar to the following:
+///
+/// ```none
+/// [alpha, bravo, charlie, delta, echo, foxtrot, golf]
+/// ```
+///
+/// This allows the tree to have better [cache locality](https://en.wikipedia.org/wiki/Locality_of_reference)
+/// inside of the CPU's [cache](https://en.wikipedia.org/wiki/CPU_cache) and the
+/// struture of the tree is implied by the position of each element inside of
+/// this list.
+///
+/// Unique to this merkle (hash) tree implementation, we also include a list of
+/// references to each bottom-most [data block](https://en.wikipedia.org/wiki/Block_(data_storage))
+/// for improved preformance when searching, which would look something like the
+/// following:
+///
+/// ```none
+/// [&charlie, &delta, &foxtrot, &golf]
+/// ```
+///
+/// This data structure may be put to use without needing to be transformed into
+/// something else in order to store the data blocks.
+pub struct Irkle<T: AsRef<[u8]>> {
+    /// Inner array used as the main storage device for the tree
+    pub inner: Vec<Node<T>>,
+
+    /// A list of referenced towards data blocks stored in [Irkle::inner], used
+    /// for quick searching and to allow better usability of the raw data
+    ///
+    /// All [Node]s of this vector are guaranteed to have an [Option::Some] value
+    /// for the [Node::data] element.
+    pub data: Vec<Weak<Node<T>>>,
+}
+
+/// Tests for [Irkle]-related operations
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // TODO: test
+}
---
+++ src/node.rs
@@ -0,0 +1,28 @@
+//! Contains [Node], see item-level documentation for more information
+
+use blake3;
+
+/// A single node on the [Irkle] tree which maps to a [blake3](https://en.wikipedia.org/wiki/BLAKE_(hash_function)#BLAKE3)-based
+/// hash and optionally some data
+///
+/// If this does contain some data, this would now be considered a "data block".
+/// If this is not the case, this node would simply be referred to as a leaf in
+/// the tree.
+pub struct Node<T: AsRef<[u8]>> {
+    /// Required hash of this node
+    pub hash: blake3::Hash,
+
+    /// Optional data contained inside of this node
+    ///
+    /// If this infomation is present (e.g. an [Option::Some] value), this node
+    /// is considered a "data block" and if not, it is assumed to be a leaf.
+    pub data: Option<T>,
+}
+
+/// Tests for [Node]-related operations
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // TODO: test
+}
