eba53610fc74aa4a5faa8582cd31fd3e7da491f0
Owez <root@ogriffiths.com>
2020-11-29 18:43:08 +0000

    Added more docs to tree items for irkle

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -51,13 +51,31 @@ use std::rc::Weak;
 /// something else in order to store the data blocks.
 #[derive(Debug, Clone)]
 pub struct Tree<T: AsRef<[u8]>> {
-    /// Inner array used as the main storage device for the tree
+    /// Inner array used as the main storage device for the tree, containing a
+    /// form of [binary tree arrays](https://en.wikipedia.org/wiki/Binary_tree#Arrays)
+    ///
+    /// If you'd like a usable list of all accessible data blocks inside of the
+    /// tree, you may want to utilise the attached [Tree::data] element, which
+    /// allows direct access.
     pub inner: Vec<Node<T>>,
 
-    /// A list of referenced towards data blocks stored in [Tree::inner], used
-    /// for quick searching and to allow better usability of the raw data
+    /// A list of referenced towards data blocks stored in [Tree::inner] and their
+    /// position inside said array, used for quick searching and to allow better
+    /// usability of the raw data
     ///
     /// All [Node]s of this vector are guaranteed to have an [Option::Some] value
-    /// for the [Node::data] element.
-    pub data: Vec<Weak<Node<T>>>,
+    /// for the [Node::data] element
+    ///
+    /// # Behind-the-scenes
+    ///
+    /// This element simply makes a new [Vec] of the elements of:
+    ///
+    /// ```none
+    /// (inner.len() / 2 + 1)..array.len()
+    /// ```
+    ///
+    /// Due to [binary tree arrays](https://en.wikipedia.org/wiki/Binary_tree#Arrays)
+    /// simply having all data blocks at the end of the array, starting from the
+    /// previously mentioned `inner.len() / 2 + 1` equation
+    pub data: Vec<(Weak<Node<T>>, usize)>,
 }
